b2824be0ecda251f88b40c40f1fe7555a5ab640a
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-13 14:14:56 +0930

    Migrate to netstandard

--- Src/Genesis.GenericSerialDisposable.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.GenericSerialDisposable.UnitTests")]
-[assembly: AssemblyDescription("Contains unit tests for Genesis.GenericSerialDisposable.")]
-[assembly: CLSCompliantAttribute(true)]
--- Src/Genesis.GenericSerialDisposable/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.GenericSerialDisposable")]
-[assembly: AssemblyDescription("Contains the implementation of Genesis.GenericSerialDisposable.")]
-[assembly: CLSCompliantAttribute(true)]
